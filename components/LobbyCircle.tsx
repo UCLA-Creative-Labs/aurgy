db52dad35519af42cf198672dcc0e7bcb6b03d12
Francis-Feng <61172425+Francis-Feng@users.noreply.github.com>
2022-02-15 17:10:01 -0800

    chore: lint
    - use escaped double quotes instead of single quotes to run lint command

--- components/LobbyCircle.tsx
+++ components/LobbyCircle.tsx
@@ -2,36 +2,36 @@ import React from 'react';
 import styles from '../styles/home.module.scss';
 
 export interface LobbyCircleProps {
-    name: string;
-    // create options
-    // 0 = not create
-    // 1 = create with no lobbies
-    // 2 = create with lobbies
-    create?: number;
-    offset?: boolean;
+  name: string;
+  // create options
+  // 0 = not create
+  // 1 = create with no lobbies
+  // 2 = create with lobbies
+  create?: number;
+  offset?: boolean;
 }
 
 export function LobbyCircle({name, create = 0, offset = false}: LobbyCircleProps): JSX.Element {
-    let finalStyle;
+  let finalStyle;
 
-    if (create == 1) {
-        finalStyle = `${styles.circle} ${styles.single}`;
-    } else if (create == 2) {
-        finalStyle = `${styles.circle} ${styles.create}`;
-    } else if (offset) {
-        finalStyle = `${styles.circle} ${styles.offset}`;
-    } else {
-        finalStyle = `${styles.circle} ${styles.top}`;
-    }
-    
-    return (
-        <a href='/' className={finalStyle}>
-            <div className={styles.name}>
-                {name}
-            </div>
-        </a>                
-        
-    )
+  if (create == 1) {
+    finalStyle = `${styles.circle} ${styles.single}`;
+  } else if (create == 2) {
+    finalStyle = `${styles.circle} ${styles.create}`;
+  } else if (offset) {
+    finalStyle = `${styles.circle} ${styles.offset}`;
+  } else {
+    finalStyle = `${styles.circle} ${styles.top}`;
+  }
+
+  return (
+    <a href='/' className={finalStyle}>
+      <div className={styles.name}>
+        {name}
+      </div>
+    </a>
+
+  );
 }
 
 export default LobbyCircle;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
-import { create } from 'domain';
 import React from 'react';
 import Layout from '../components/Layout';
 import LobbyCircle  from '../components/LobbyCircle';
 import styles from '../styles/home.module.scss';
 
 const SAMPLE_LOBBIES = [
-  'jeff', 
-  'annie', 
-  'troy', 
+  'jeff',
+  'annie',
+  'troy',
   'abed',
   'britta',
   'shirley',
@@ -18,7 +17,7 @@ const SAMPLE_LOBBIES = [
   'magnitude',
   'ian',
   'buzz',
-]
+];
 
 export default function Home(): JSX.Element {
   let empty = true;
@@ -26,15 +25,15 @@ export default function Home(): JSX.Element {
     empty = false;
   }
 
-  let createButton = empty ? <LobbyCircle name='CREATE' create={1}/> 
-                           : <LobbyCircle name='CREATE' create={2}/>
+  const createButton = empty ? <LobbyCircle name='CREATE' create={1}/>
+    : <LobbyCircle name='CREATE' create={2}/>;
 
   return (
     <Layout>
       <div className={styles.container}>
         {createButton}
         {SAMPLE_LOBBIES.map((lobby, index) => (
-          <LobbyCircle name={lobby} offset={!index}/>  // offset only applied to the first element
+          <LobbyCircle name={lobby} offset={!index} key={index}/>  // offset only applied to the first element
         ))}
       </div>
     </Layout>
