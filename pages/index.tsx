17b4369c2689a40eb5efa0f3f05eaae33dcb50a6
Francis-Feng <61172425+Francis-Feng@users.noreply.github.com>
2022-02-15 16:36:34 -0800

    feat: add home page
    - create LobbyCircle component
    - set up home page grid
    - implement horizontal scroll
    
    - TODO: replace solid colors with static shaders
    - TODO: wrap text around circumference of LobbyCircle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,42 @@
+import { create } from 'domain';
 import React from 'react';
 import Layout from '../components/Layout';
+import LobbyCircle  from '../components/LobbyCircle';
+import styles from '../styles/home.module.scss';
+
+const SAMPLE_LOBBIES = [
+  'jeff', 
+  'annie', 
+  'troy', 
+  'abed',
+  'britta',
+  'shirley',
+  'pierce',
+  'chang',
+  'craig',
+  'starburns',
+  'magnitude',
+  'ian',
+  'buzz',
+]
 
 export default function Home(): JSX.Element {
+  let empty = true;
+  if (SAMPLE_LOBBIES.length > 0) {
+    empty = false;
+  }
+
+  let createButton = empty ? <LobbyCircle name='CREATE' create={1}/> 
+                           : <LobbyCircle name='CREATE' create={2}/>
 
   return (
     <Layout>
-      <h1>AURGY</h1>
+      <div className={styles.container}>
+        {createButton}
+        {SAMPLE_LOBBIES.map((lobby, index) => (
+          <LobbyCircle name={lobby} offset={!index}/>  // offset only applied to the first element
+        ))}
+      </div>
     </Layout>
   );
 }
