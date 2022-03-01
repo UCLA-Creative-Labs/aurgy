31255e47ceb11f16caac63c7254b6dae79843583
Francis-Feng <61172425+Francis-Feng@users.noreply.github.com>
2022-03-01 14:54:33 -0800

    chore: use conventions and refactor
    - use enums
    - improve code readability by refactoring
    - separate home and LobbyCircle styles
    - remove unnecessary bootstrap package
    - change eslint "no-shadow" rule to allow for TypeScript enums

--- components/LobbyCircle.tsx
+++ components/LobbyCircle.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import styles from '../styles/home.module.scss';
+import styles from '../styles/LobbyCircle.module.scss';
+
+enum Create {
+  NotCreate,
+  CreateOnly,
+  CreateWithLobbies
+}
 
 export interface LobbyCircleProps {
   name: string;
-  // create options
-  // 0 = not create
-  // 1 = create with no lobbies
-  // 2 = create with lobbies
   create?: number;
-  offset?: boolean;
 }
 
-export function LobbyCircle({name, create = 0, offset = false}: LobbyCircleProps): JSX.Element {
+export function LobbyCircle({name, create = Create.NotCreate}: LobbyCircleProps): JSX.Element {
+
   let finalStyle;
 
-  if (create == 1) {
-    finalStyle = `${styles.circle} ${styles.single}`;
-  } else if (create == 2) {
-    finalStyle = `${styles.circle} ${styles.create}`;
-  } else if (offset) {
-    finalStyle = `${styles.circle} ${styles.offset}`;
+  if (create == Create.CreateOnly) {
+    finalStyle = styles.single;
+  } else if (create == Create.CreateWithLobbies) {
+    finalStyle = styles.create;
   } else {
-    finalStyle = `${styles.circle} ${styles.top}`;
+    finalStyle = styles.top;
   }
 
   return (
-    <a href='/' className={finalStyle}>
+    <a href='/' className={`${styles.circle} ${finalStyle}`}>
       <div className={styles.name}>
         {name}
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,20 +20,14 @@ const SAMPLE_LOBBIES = [
 ];
 
 export default function Home(): JSX.Element {
-  let empty = true;
-  if (SAMPLE_LOBBIES.length > 0) {
-    empty = false;
-  }
-
-  const createButton = empty ? <LobbyCircle name='CREATE' create={1}/>
-    : <LobbyCircle name='CREATE' create={2}/>;
+  const createButton = <LobbyCircle name='CREATE' create={!SAMPLE_LOBBIES.length ? 1 : 2}/>;
 
   return (
     <Layout>
       <div className={styles.container}>
         {createButton}
         {SAMPLE_LOBBIES.map((lobby, index) => (
-          <LobbyCircle name={lobby} offset={!index} key={index}/>  // offset only applied to the first element
+          <LobbyCircle name={lobby} key={index}/>
         ))}
       </div>
     </Layout>
