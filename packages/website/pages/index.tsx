65c96d05dfd22a81fd12d5848a17e687d5b0bb6c
Bryan Pan <bryanpan342@gmail.com>
2022-04-14 22:45:08 -0700

    linters

--- packages/website/pages/index.tsx
+++ packages/website/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Layout from '../components/Layout';
 import LobbyCircle  from '../components/LobbyCircle';
 import styles from '../styles/home.module.scss';
