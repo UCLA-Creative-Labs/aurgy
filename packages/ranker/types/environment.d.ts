b4c8666d1ab1de2bbb189f141bf26fac20428d4a
Thilan Tran <thilanoftran@gmail.com>
2022-05-02 11:12:24 -0700

    chore: fix lints

--- packages/ranker/types/environment.d.ts
+++ packages/ranker/types/environment.d.ts
@@ -1,8 +1,10 @@
-namespace NodeJS {
-  interface ProcessEnv extends NodeJS.ProcessEnv {
-    CLIENT_ID: string;
-    CLIENT_SECRET: string;
-    SECRET: string;
-    NEXTAUTH_URL: string;
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends NodeJS.ProcessEnv {
+      CLIENT_ID: string;
+      CLIENT_SECRET: string;
+      SECRET: string;
+      NEXTAUTH_URL: string;
+    }
   }
 }
